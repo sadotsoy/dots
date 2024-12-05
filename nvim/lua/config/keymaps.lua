7b4039d48e0190dbd358eeb0356fe577e4a8d2a6
sadotc <sadotcortes@gmail.com>
2024-12-05 11:00:06 -0600

    chore(keymap): add keymap to split new buffer to vertical

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- windows
+--- map to create a new window in vertical
+vim.keymap.set(
+  "n",
+  "<leader>ws",
+  "<cmd>vnew<CR>",
+  { noremap = true, silent = true, desc = "Create new window vertical" }
+)
