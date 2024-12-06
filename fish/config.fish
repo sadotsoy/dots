ea172bb675201017aa13850b0ca3393092acc009
sadot <sadotcortes@gmail.com>
2024-12-06 09:55:49 -0600

    chore(fish:config): updating shellenv path for brew

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# check if homebrew is installed and add it to the path
+if type -q /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+else if type -q /usr/local/bin/brew
+    eval (/usr/local/bin/brew shellenv)
+end
