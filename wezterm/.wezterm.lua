2e4cb9f97ea3c79fc9f448b5dcd81f5b7ecd43b8
sadotc <sadotcortes@gmail.com>
2024-12-06 21:49:22 -0600

    chore(wezterm:colors): add catppucin theme mocha

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,10 +4,9 @@ local config = wezterm.config_builder()
 
 -- font styles
 config.font_size = 14.2
--- Agave
+config.font = wezterm.font("SauceCodePro Nerd Font Mono") -- font size recommended 14.2
 -- config.font = wezterm.font("Agave Nerd Font Mono") -- font size recommended 15.5
 -- config.font = wezterm.font("Hack Nerd Font Mono") -- font size recommended 14.2
-config.font = wezterm.font("SauceCodePro Nerd Font Mono") -- font size recommended 14.2
 
 -- ui
 config.window_padding = {
@@ -17,35 +16,15 @@ config.window_padding = {
 	bottom = 0,
 }
 config.force_reverse_video_cursor = true
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.97
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 
---colors
-local kanagawaDragon = {
-	foreground = "#dcd7ba",
-	background = "#181616",
-
-	cursor_bg = "#c8c093",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
-
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
-
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
-
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-}
-
 -- colorscheme
--- config.colors = kanagawaDragon
-
+config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "Kasugano (terminal.sexy)"
-config.color_scheme = "3024 (dark) (terminal.sexy)"
+-- config.color_scheme = "3024 (dark) (terminal.sexy)"
+-- config.color_scheme = "Catppuccin Mocha (Gogh)"
 -- config.color_scheme = 'Kanagawa Dragon (Gogh)'
 
 return config
