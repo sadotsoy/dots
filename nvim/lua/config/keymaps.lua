f9424ceb2359f4f87919f09788716c9bcdd06680
sadotc <sadotcortes@gmail.com>
2024-12-06 21:48:40 -0600

    chore(lazy:keymaps): add keymaps to rezize window

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,25 @@ vim.keymap.set(
   "<cmd>vnew<CR>",
   { noremap = true, silent = true, desc = "Create new window vertical" }
 )
+
+-- maps to resize windows
+vim.keymap.set("n", "<C-3>", "<cmd>resize +2<CR>", { noremap = true, silent = true, desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-4>", "<cmd>resize -2<CR>", { noremap = true, silent = true, desc = "Decrease Window Height" })
+vim.keymap.set(
+  "n",
+  "<C-1>",
+  "<cmd>vertical resize -2<CR>",
+  { noremap = true, silent = true, desc = "Decrease Window Width" }
+)
+vim.keymap.set(
+  "n",
+  "<C-2>",
+  "<cmd>vertical resize +2<CR>",
+  { noremap = true, silent = true, desc = "Increase Window Width" }
+)
+
+-- delete lazy keymaps to resize the window
+vim.keymap.del("n", "<C-Up>")
+vim.keymap.del("n", "<C-Down>")
+vim.keymap.del("n", "<C-Left>")
+vim.keymap.del("n", "<C-Right>")
