681f3124769c81ab44946ecb6f3a016fff463965
sadot <sadotcortes@gmail.com>
2024-12-09 14:34:18 -0600

    chore(fish:glist): remove

--- fish/functions/glist.fish
+++
@@ -1,4 +0,0 @@
-function glist --wraps='git log --format="- %h %s" --oneline' --description 'alias glist=git log --format="- %h %s" --oneline'
-  git log --format="- %h %s" --oneline $argv
-        
-end
