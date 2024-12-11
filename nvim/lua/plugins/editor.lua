b67d95452c5745d67cb9fe47c70b60b88728bc22
sadotc <sadotcortes@gmail.com>
2024-12-10 18:44:09 -0600

    chore(editor:neotree): show all hidden files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -65,6 +65,11 @@ return {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
       },
       window = {
         mappings = {
